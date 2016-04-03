d725f4f012b013077c20a66ffd9f2a19e3729e98
RanaAlshemali <ra2834a@student.american.edu>
2016-04-03 02:19:13 -0400

    display all media

--- browse.php
+++ browse.php
@@ -81,7 +81,38 @@ if (! $result) {
 				onclick="javascript:saveDownload(<?php echo $result_row[4];?>);">Download</a>
 			</td>
 			<td>  <?php
-			echo $username; // mediaid
+	if(substr($type,0,5)=="image") //view image
+	{
+		echo "Viewing Picture:";
+		echo $result_row[4];
+		echo "<img src='".$filepath."'/>";
+	}
+	else //view movie
+	{	
+?>
+	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
+	<p>Viewing Video:<?php echo $result_row[4];?></p>
+	      
+    <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
+
+<param name="filename" value="<?php echo $result_row[4];?>">
+	<!-- echo $result_row[2].$result_row[1];  -->
+		
+
+<param name="Showcontrols" value="True">
+<param name="autoStart" value="True">
+
+<embed type="application/x-mplayer2" src="<?php echo $filepath;  ?>" name="MediaPlayer" width=320 height=240></embed>
+
+</object>
+
+          
+          
+          
+       
+              
+<?php
+	}
 			?>
 			</td>
 		</tr>
