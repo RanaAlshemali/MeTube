ce2d799209baf40b589292542591a25192608147
RanaAlshemali <ra2834a@student.american.edu>
2016-04-03 20:44:47 -0400

    same file

--- browse.php
+++ browse.php
@@ -99,7 +99,7 @@ if (! $result) {
 
 				<object id="MediaPlayer" width=320 height=286
 					classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95"
-					standby="Loading Windows Media Player componentsâ€¦"
+					standby="Loading Windows Media Player components…"
 					type="application/x-oleobject"
 					codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
