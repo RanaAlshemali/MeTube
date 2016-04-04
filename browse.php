c8ed7ed7510e2532f3a256539118e45cda84246d
RanaAlshemali <ra2834a@student.american.edu>
2016-04-04 00:27:07 -0400

    logout

--- browse.php
+++ browse.php
@@ -26,6 +26,7 @@ function saveDownload(id)
 
 <body>
 	<p>Welcome <?php echo $_SESSION['username'];?></p>
+	<a href="logout.php">logout</a>
 	<a href='media_upload.php' style="color: #FF9900;">Upload File</a>
 	<div id='upload_result'>
 <?php
