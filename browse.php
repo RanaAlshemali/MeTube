f5f4fe9bf1a0f11b314ece70db1cf9f1daaf8c8a
RanaAlshemali <ra2834a@student.american.edu>
2016-04-04 01:56:01 -0400

    test video

--- browse.php
+++ browse.php
@@ -111,9 +111,9 @@ if (! $result) {
 				?>
 	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
 
-<video controls>
+<video controls width=320 height=286>
 
-    <source src="<?php echo $result_row[4];?>#t=6,15">
+    <source src="uploads/ralshem/My+Movie.mp4#t=6,15">
 </video>
 
 				<object id="MediaPlayer" width=320 height=286
