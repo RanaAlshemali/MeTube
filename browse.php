a8ca9b8d923306c456042a2cb20e10f0355a368b
RanaAlshemali <ra2834a@student.american.edu>
2016-04-04 01:53:27 -0400

    test video

--- browse.php
+++ browse.php
@@ -111,6 +111,10 @@ if (! $result) {
 				?>
 	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
 
+<video controls>
+
+    <source src="<?php echo $result_row[4];?>#t=6,15">
+</video>
 
 				<object id="MediaPlayer" width=320 height=286
 					classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95"
