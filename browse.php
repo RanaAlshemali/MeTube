796a7ff079ee71c34806e0f69b7bb65f30ef5e51
RanaAlshemali <ra2834a@student.american.edu>
2016-04-04 01:57:59 -0400

    hide controls

--- browse.php
+++ browse.php
@@ -111,7 +111,7 @@ if (! $result) {
 				?>
 	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
 
-<video controls width=320 height=286>
+<video  width=320 height=286>
 
     <source src="uploads/ralshem/My+Movie.mp4">
 </video>
