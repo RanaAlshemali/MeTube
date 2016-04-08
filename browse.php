54088454853bd2a9ee2096643235130d86c19163
RanaAlshemali <ra2834a@student.american.edu>
2016-04-07 22:52:08 -0400

    top:50%;    left:50%;    margin-top:-25px;    margin-left:-25px;

--- browse.php
+++ browse.php
@@ -24,7 +24,7 @@ function saveDownload(id)
 } 
 </script>
 <style >
-#image1{
+#image11{
     position:relative;
    top:50%;
    left:50%;
@@ -122,7 +122,7 @@ if (! $result) {
 				else{ // view movie
 
 					?>
-					<div id="image1">
+					<div >
 	<!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
  
 				
@@ -131,7 +131,7 @@ if (! $result) {
 				<video width="320" height="286" style="position: absolute"> 
 				<source src="<?php echo $result_row[4];?>#t=15">  
 				</video>
-				<?php echo "\n <img id='image1' src='uploads/video-icon-150x150.png' height='100' width='100'/>";?>
+				<?php echo "\n <img id='image11' src='uploads/video-icon-150x150.png' height='100' width='100'/>";?>
 				
 				</a>
          
