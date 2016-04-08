a52080c907eb9a225d732c0474cde2162be0320f
RanaAlshemali <ra2834a@student.american.edu>
2016-04-08 02:53:50 -0400

    $filename

--- browse.php
+++ browse.php
@@ -99,6 +99,9 @@ if (! $result) {
 				$filenpath = $result_row [4];
 				$thumbnail= $result_row [5];
 				$dateCreated = $result_row [6];
+				if(trlen($filename)>20){
+					$filename= substr ( $filename, 0, 20 );
+				}
 				echo '<br />';
 				echo '<br />';
 				echo $mediaid . "  "; // mediaid
