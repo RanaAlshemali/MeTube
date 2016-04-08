5b2c68a8b7b873354342eda2d6e288850194cef3
RanaAlshemali <ra2834a@student.american.edu>
2016-04-08 03:15:40 -0400

    div

--- browse.php
+++ browse.php
@@ -110,7 +110,7 @@ if (! $result) {
 				?>
 			
 			<a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
-			<h6 align="right">Type: <?php echo $type;  echo '<br />';?></h6>
+			<div align="right">Type: <?php echo $type;  echo '<br />';?></div>
 			 <?php
 				echo '<br />';
 				if ( $type == "image"){ // view image
