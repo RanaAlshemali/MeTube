33be04ae9f9dee7705b6e514edd4fedadbaf55af
RanaAlshemali <ra2834a@student.american.edu>
2016-04-10 01:47:06 -0400

    <?php 	 $insert = "insert into favList(favid, mediaid,username)". 	 		"values(NULL,'".$mediaid ."','" .$username ."')"; 	 $queryresult = mysql_query($insert) 	 or die("Insert into Media error in media_upload_process.php " .mysql_error()); ?>

--- browse.php
+++ browse.php
@@ -158,7 +158,7 @@ if (! $result) {
 		 <?php echo '<br />';?>
 			<div>
 			<div  style="float: left;">Created On: <?php echo substr( $dateCreated, 0, 10 ); echo '<br />';?></div>
-			<div  style="float: right;"><img src="uploads/ralshem/Star-Full.png" height="20" width="20" onClick="addFav('1','2')"/></div>
+			<div  style="float: right;"><img src="uploads/ralshem/Star-Full.png" height="20" width="20" onClick="addFav('<?php echo  $mediaid;?>','<?php echo $_SESSION['username'];?>')"/></div>
 			</div>
 			 <?php 	
 			}?>
@@ -169,9 +169,14 @@ if (! $result) {
 		?>
 		</table>
 <script>
-function addFav(a,b) {
-	 
-alert("This Media is Successfully Added to Your Favorite List" + a+b);
+function addFav($mediaid,$username) {
+	 <?php 	
+	 $insert = "insert into favList(favid, mediaid,username)".
+	 		"values(NULL,'".$mediaid ."','" .$username ."')";
+	 $queryresult = mysql_query($insert)
+	 or die("Insert into Media error in media_upload_process.php " .mysql_error());
+?>
+alert("This Media is Successfully Added to Your Favorite List");
 }
 
 </script>
