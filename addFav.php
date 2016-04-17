767efea457b93f68fd3662069f5f51d66c7b6ed9
RanaAlshemali <ra2834a@student.american.edu>
2016-04-16 23:43:08 -0400

    if(function_exists($_GET['f'])) {    $_GET['f'](); }

--- addFav.php
+++ addFav.php
@@ -1,13 +1,16 @@
 
 	 <?php 	
 include_once "function.php";
-	 
+if(function_exists($_GET['f'])) {
+	$_GET['f']();
+}
+function addFav(){	 
 	$mediaid = $_GET['id'];
 	$username = $_GET['username'];
 	$insert = "insert into favList(favid, mediaid,username)".
 	"values(NULL,'".$mediaid ."','" .$username ."')";
 	$queryresult = mysql_query($insert)
 	or die("Insert into Media error in media_upload_process.php " .mysql_error());
-
+}
 ?>
  
