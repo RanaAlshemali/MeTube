0dc191d9b04fe6adee10cce0a31bf5cd26a652e3
RanaAlshemali <ra2834a@student.american.edu>
2016-04-17 05:07:19 -0400

    add red star for fav

--- browse.php
+++ browse.php
@@ -219,7 +219,7 @@ if (! $result) {
 			<div>
 			<div  style="float: left;">Created On: <?php echo substr( $dateCreated, 0, 10 ); echo '<br />';?></div>
 			<div  id="<?php echo  $mediaid;?>" style="float: right;">
-			<?php if (mysql_num_rows ( $FavListresult)==0 ){?>
+			<?php if (mysql_num_rows ( $FavListresult)==1 ){?>
 			<img id="<?php echo  $mediaid;?>" src="uploads/kevin/red-star.png" height="20" width="20" onClick="javascript:add(this.id)"/></div>
 			<?php }else {?>
 				<img id="<?php echo  $mediaid;?>" src="uploads/ralshem/Star-Full.png" height="20" width="20" onClick="javascript:add(this.id)"/></div>
