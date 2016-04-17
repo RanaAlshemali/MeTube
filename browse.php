e4a0371e68b606e94db9cc59d3226d0f4a905567
RanaAlshemali <ra2834a@student.american.edu>
2016-04-17 04:58:13 -0400

    $currentuser

--- browse.php
+++ browse.php
@@ -160,7 +160,7 @@ if (! $result) {
 					$filename= substr ( $filename, 0, 20 );
 				}
 				$type = substr ( $type, 0, 5 );
-				$queryFavList = "SELECT * from favList WHERE username = $currentuserm AND mediaid= $mediaid ";
+				$queryFavList = "SELECT * from favList WHERE username = $currentuser AND mediaid= $mediaid ";
 				$FavListresult = mysql_query ( $queryFavList );
 				if ( $FavListresult == Null) {
 					die ( "not <br />" . mysql_error () );
