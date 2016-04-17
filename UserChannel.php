baf9d02a77b0031ebb17c1795fba6a7bd2d85bb7
RanaAlshemali <ra2834a@student.american.edu>
2016-04-17 15:00:40 -0400

    delete media

--- UserChannel.php
+++ UserChannel.php
@@ -225,14 +225,8 @@ if (! $result) {
 			<div>
 			<div  style="float: left;">Created On: <?php echo substr( $dateCreated, 0, 10 ); echo '<br />';?></div>
 			<div  id="<?php echo  $mediaid;?>" style="float: right;"> <?php echo  $count;?>
-			<img id="<?php echo  $mediaid;?>" src="uploads/delete.png" height="20" width="20" onClick="javascript:del(this.id)"/></div>
-			
-			<?php if (mysql_num_rows ( $FavListresult)==1 ){?>
-			<img id="<?php echo  $mediaid;?>" src="uploads/red-star.png" height="20" width="20" onClick="javascript:del(this.id)"/></div>
-			<?php }else {?>
-				<img id="<?php echo  $mediaid;?>" src="uploads/Star-Full.png" height="20" width="20" onClick="javascript:add(this.id)"/></div>
-			<?php		
-			}?></div>
+			<img  src="uploads/delete.png" height="20" width="20" onClick="javascript:del(this.id)"/></div>
+			</div>
 			
 			</div>
 			 <?php 	
