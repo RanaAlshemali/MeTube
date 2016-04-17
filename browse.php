1a3e96db4b5cb7c0c77b24c2b71633b91faf5803
RanaAlshemali <ra2834a@student.american.edu>
2016-04-17 01:20:36 -0400

    <?php
    		$insert = "insert into favList(favid, mediaid,username)".
    		"values(NULL,'id','" .$_SESSION['username'] ."')";
    		$queryresult = mysql_query($insert)
    		or die("Insert into Media error in media_upload_process.php "
    .mysql_error());
    		?>

--- browse.php
+++ browse.php
@@ -228,7 +228,13 @@ if (! $result) {
  <script  >
  function addFav(id){	 
 	 alert("In");
-		
+	 <?php 	
+				
+				$insert = "insert into favList(favid, mediaid,username)".
+				"values(NULL,'id','" .$_SESSION['username'] ."')";
+				$queryresult = mysql_query($insert)
+				or die("Insert into Media error in media_upload_process.php " .mysql_error());
+				?>
 		alert("Added");
 		}
 	alert("out");
