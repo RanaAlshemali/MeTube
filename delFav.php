8a962f19ad1485caaa24c805edee86db2ed46ff5
RanaAlshemali <ra2834a@student.american.edu>
2016-04-17 05:15:25 -0400

    $delete fav

--- delFav.php
+++ delFav.php
@@ -7,7 +7,7 @@ $username=$_SESSION['username'];
 $id = $_GET['id'];
  
 	$delete = "delete * from favList where mediaid ='". $id . "' and username='" .$username."')";
-	$queryresult = mysql_query($insert)
+	$queryresult = mysql_query($delete)
 	or die("Delete from favList in delFac.php " .mysql_error());
 
 		header('Location: browse.php');
