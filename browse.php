600de55c47b3251b4d5f2a28bb5435fc94e75634
RanaAlshemali <ra2834a@student.american.edu>
2016-04-18 00:07:35 -0400

    add <meta charset="UTF-8">

--- browse.php
+++ browse.php
@@ -8,7 +8,7 @@ else
 	$currentuser= $_SESSION['username'];
 include_once "function.php";
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+ 
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
