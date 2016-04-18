2a8139892ca1fbf76704c4335fd961d9be19edfc
RanaAlshemali <ra2834a@student.american.edu>
2016-04-18 14:14:51 -0400

    $playlist_num_rows

--- browse.php
+++ browse.php
@@ -116,6 +116,7 @@ $where.= ' AND lower(concat(username, \'\', filename, \'\', dateCreated, \'\', k
 $queryplaylist = "SELECT DISTINCT(playlistname) AS 	playlistname from playList Where username= '".$currentuser."'";
 $playlistresult = mysql_query ( $queryplaylist );
 $playlist_num_rows = mysql_num_rows ( $playlistresult );
+echo "********".$playlist_num_rows;
 $playlist_rows = mysql_fetch_row ( $playlistresult );
 
 if ($where == "WHERE catagory = ''"){ //All catagories
@@ -252,7 +253,14 @@ if (! $result) {
 			 <option value="addtoplaylist">Add to test</option>
 			 <option value="addtoplaylist">Add to test</option>
 			 <option value="addtoplaylist">Add to test</option>
-		 			
+		 			<?php 	
+			if($username == $currentuser){
+			 for($k=0; k<$playlist_num_rows; $k++){
+			 $playlistName = $playlist_rows[0];
+			 	?>
+ 	 <option value="<?php echo  $playlistName;?>"><?php echo  $playlistName;?></option>
+ <?php }
+			} ?>
 
 		</select>
 			</div>
