cae2cad8e0efc1a5617187e04781ba213a2c5a8f
RanaAlshemali <ra2834a@student.american.edu>
2016-04-18 14:42:54 -0400

    <option value="addtoplaylist">Add to test</option>

--- browse.php
+++ browse.php
@@ -240,8 +240,7 @@ if (! $result) {
 			<div>
 			<select name="addplaylist">
 			 <option value="addtoplaylist">Add to Playlist</option>
-			 <option value="addtoplaylist">Add to test</option>
-			 <option value="addtoplaylist">Add to test</option>
+			 
 
 <?php 	
 			if(!($currentuser == "")){
@@ -252,8 +251,7 @@ if (! $result) {
  	 <option value="<?php echo  $playlistName;?>">Add to <?php echo  $playlistName;?></option>
  <?php }
 			} ?>
-			 <option value="<?php echo  $playlistName;?>">Add to <?php echo  $playlistName;?></option>
-
+			<option value="createplaylist">Create Playlist</option>
 		</select>
 			</div>
 			 <?php 	
