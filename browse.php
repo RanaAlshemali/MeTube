a5f366d3f66e7e7d8929dbf764d00166d1aaac4e
RanaAlshemali <ra2834a@student.american.edu>
2016-04-19 20:17:31 -0400

    fix ff

--- browse.php
+++ browse.php
@@ -17,9 +17,9 @@ include_once "function.php";
 <title>Media browse</title>
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <script type="text/javascript" src="js/jquery-latest.pack.js"></script>
-   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<link rel="stylesheet" href="assets/css/main.css" />
  
+<link rel="stylesheet" href="assets/css/main.css" />
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <style >
 
 </style>
