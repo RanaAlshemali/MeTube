35ed0e20c9503b0873c5f542a703c3f7d9a79d87
RanaAlshemali <ra2834a@student.american.edu>
2016-04-19 21:02:17 -0400

    ..

--- browse.php
+++ browse.php
@@ -13,11 +13,11 @@ include_once "function.php";
  
 <head>
 <meta charset="UTF-8">
-
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Media browse</title>
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <script type="text/javascript" src="js/jquery-latest.pack.js"></script>
-
+   <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <link rel="stylesheet" href="assets/css/main.css" />
  
 <style >
